a8b37b63bc7c1c00a5546614bdecf37a86de5c12
juanocasio <juanocasio@comcast.net>
2017-02-09 21:41:55 -0500

    imported http

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-import config, {nodeENV} from './config';
+import http from 'http';
 
-console.log(config, nodeENV);
+const server = http.createServer((req,res)=> {
+  res.write('Hello HTTP!\n');
+  setTimeout(() => {
+    res.write('I can stream!\n');
+    res.end();
+  }, 3000);
+});
+
+server.listen(8080);
+
