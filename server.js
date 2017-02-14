5a2e1c1ad12a23bd7ccdfb4ca648ff4936b5e9b5
juanocasio <juanocasio@comcast.net>
2017-02-14 17:03:59 -0500

    updates to components

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import config from './config';
-import express from 'express'
-import fs from 'fs';
+import apiRouter from './api';
 
+import express from 'express'
 const server = express();
 
-server.get('/', (req,res) => {
-  res.send('Hello Express');
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index', {
+    content: '...'
+  });
 });
 
-// server.get('/about.html', (req,res) => {
-//   fs.readFile('./about.html', (err,data)=>{
-//     res.send(data.toString());
-//   });
-// });
+server.use('/api', apiRouter);
 
 server.use(express.static('public'));
 
