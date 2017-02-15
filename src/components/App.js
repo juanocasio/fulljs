70d79ba11e78b744b9c7de23f657fe202225f5c7
juanocasio <juanocasio@comcast.net>
2017-02-14 21:21:36 -0500

    added new content to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
 import React from 'react'
-
 import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+import axios from 'axios';
+
+
 
 class App extends React.Component{
     state = {
-        pageHeader: 'Naming Contest'
+        pageHeader: 'Naming Contest',
+        contests: []
     };
+    componentDidMount() {
+        axios.get('/api/contests')
+        .then(resp => {
+            this.setState({
+                contests: resp.data.contests
+            }); 
+        })
+        .catch(console.error)
+ 
+    }
     render(){
         return (
             <div className="App">
                 <Header message={this.state.pageHeader} />
                 <div>
-                   ...
+                    {this.state.contests.map(contest =>
+                        <ContestPreview key={contest.id}{...contest} />
+                    )}
                 </div>
             </div>
         );
